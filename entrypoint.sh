a9f8ed5456826db32e6691b826618218dbbc66e0
Avinash <avinash.kumar@hashedin.com>
2020-07-14 01:04:29 +0530

    added somechanges to github actions

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]]; then
+	EVENT_ACTION=$(jq -r ".action" "${GITHUB_EVENT_PATH}")
+	if [[ "${EVENT_ACTION}" != "opened" ]]; then
+		echo "No need to run analysis. It is already triggered by the push event."
+		exit 78
+	fi
+fi
+
+[[ ! -z ${INPUT_PASSWORD} ]] && SONAR_PASSWORD="${INPUT_PASSWORD}" || SONAR_PASSWORD=""
+
+sonar-scanner \
+	-Dsonar.host.url=${INPUT_HOST} \
+	-Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
+	-Dsonar.login=${INPUT_LOGIN} \
+	-Dsonar.password=${INPUT_PASSWORD} \
+	-Dsonar.sources=. \
+	-Dsonar.sourceEncoding=UTF-8 \
+	${SONAR_PASSWORD}
