fe6b323c5847e65114259a413afb914197a3367f
Mohammed Yahya <my.almusaddar@gmail.com>
2020-08-26 09:40:00 +0300

    add projectKey

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,7 @@ fi
 sonar-scanner \
 	-Dsonar.host.url=${INPUT_HOST} \
 	-Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
+	-Dsonar.projectKey=${INPUT_PROJECTKEY} \
 	-Dsonar.login=${INPUT_LOGIN} \
 	-Dsonar.password=${INPUT_PASSWORD} \
 	-Dsonar.sources=. \
